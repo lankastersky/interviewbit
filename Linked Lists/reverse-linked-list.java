1fedf4a9994d2a16c8efb9db0a908b5ee8920996
Anton Popov <lankastersky@gmail.com>
2018-12-31 17:45:36 -0800

    Create reverse-linked-list.java

---
+++ Linked Lists/reverse-linked-list.java
@@ -0,0 +1,37 @@
+/*
+everse Linked List
+
+Reverse a linked list. Do it in-place and in one-pass.
+
+For example:
+Given 1->2->3->4->5->NULL,
+
+return 5->4->3->2->1->NULL.
+
+https://www.interviewbit.com/problems/reverse-linked-list/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode A) {
+        ListNode prev = null;
+        ListNode next;
+        ListNode head = A;
+        ListNode current = head;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+}
