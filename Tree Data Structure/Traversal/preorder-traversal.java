dea54cd91011addd7c5417fabc31de80d8d49b65
Anton Popov <lankastersky@gmail.com>
2019-07-06 17:11:45 -0700

    Create preorder-traversal.java

---
+++ Tree Data Structure/Traversal/preorder-traversal.java
@@ -0,0 +1,51 @@
+/*
+Preorder Traversal
+
+Given a binary tree, return the preorder traversal of its nodes’ values.
+
+Example :
+Given binary tree
+
+   1
+    \
+     2
+    /
+   3
+return [1,2,3].
+
+Using recursion is not allowed.
+
+https://www.interviewbit.com/problems/preorder-traversal/
+*/
+
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> preorderTraversal(TreeNode A) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(A);
+        ArrayList<Integer> res = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return res;
+    }
+}
